cd99edbe6ec33946aad0a325b7f2b5c447b26b45
Sosina Tatyana <fedorova_t95@mail.ru>
2022-03-09 14:02:29 +0300

    Add traffic signal switching logic

--- Traffic light/ViewController.swift
+++ Traffic light/ViewController.swift
@@ -8,12 +8,68 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    enum ColorTrafficLight {
+        case red
+        case yellow
+        case green
+    }
+    
+    // MARK: - Private property
+    
+    @IBOutlet weak private var trafficLightView: UIView!
+    @IBOutlet weak private var redTrafficLight: UIView!
+    @IBOutlet weak private var yellowTrafficLight: UIView!
+    @IBOutlet weak private var greenTrafficLight: UIView!
+    @IBOutlet weak private var buttonTrafficLight: UIButton!
+    private var switchColorTrafficLight: ColorTrafficLight = .red
+    
+    // MARK: - Internal func
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        defaultSettingsTrafficLight()
     }
+    
+    // MARK: - Private func
 
-
+    @IBAction private func buttonSwitchColorTrafficLight() {
+        switchColorTrafficLightButton()
+    }
+    
+    private func defaultSettingsTrafficLight() {
+        trafficLightView.layer.cornerRadius = 22
+        
+        redTrafficLight.layer.cornerRadius = redTrafficLight.frame.height / 2
+        redTrafficLight.backgroundColor = .red.withAlphaComponent(0.3)
+        
+        yellowTrafficLight.layer.cornerRadius = yellowTrafficLight.frame.height / 2
+        yellowTrafficLight.backgroundColor = .yellow.withAlphaComponent(0.3)
+        
+        greenTrafficLight.layer.cornerRadius = greenTrafficLight.frame.height / 2
+        greenTrafficLight.backgroundColor = .green.withAlphaComponent(0.3)
+        
+        buttonTrafficLight.layer.cornerRadius = 13
+        buttonTrafficLight.setTitle("Start", for: .normal)
+    }
+    
+    private func switchColorTrafficLightButton() {
+        buttonTrafficLight.setTitle("Next", for: .normal)
+        
+        switch switchColorTrafficLight {
+        case .red:
+            redTrafficLight.backgroundColor = .red
+            greenTrafficLight.backgroundColor = .green.withAlphaComponent(0.3)
+            switchColorTrafficLight = .yellow
+        case .yellow:
+            yellowTrafficLight.backgroundColor = .yellow
+            redTrafficLight.backgroundColor = .red.withAlphaComponent(0.3)
+            switchColorTrafficLight = .green
+        case .green:
+            greenTrafficLight.backgroundColor = .green
+            yellowTrafficLight.backgroundColor = .yellow.withAlphaComponent(0.3)
+            switchColorTrafficLight = .red
+        }
+    }
 }
 
